1465ff84918df3629c7f3047e63785366c0f89ff
lysonjeada <contatotechlys@gmail.com>
2021-08-06 19:55:07 -0300

    Adiciona verificacoes de nome e cpf

--- app-formulario/ViewController.swift
+++ app-formulario/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var cpfTextField: UITextField!
+    
+    @IBAction func adicionarButton(_ sender: Any) {
+        if (nomeTextField.text!.isEmpty) {
+            nomeTextField.backgroundColor = .red
+        }
+        if (cpfTextField.text!.isEmpty) {
+            cpfTextField.backgroundColor = .red
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
